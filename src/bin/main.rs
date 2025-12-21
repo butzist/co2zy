4b7b9b8402b09e62e02070958397876008611f09
Adam M. Szalkowski <adam@szalkowski.de>
2025-12-21 19:00:50 +0100

    feat: oled

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,8 +9,19 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use embedded_graphics::mono_font::{MonoTextStyleBuilder, ascii::FONT_6X10};
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::prelude::*;
+use embedded_graphics::text::{Baseline, Text};
 use esp_hal::clock::CpuClock;
+use esp_hal::i2c::master::{Config as I2cConfig, I2c};
+use esp_hal::time::Rate;
 use esp_hal::timer::timg::TimerGroup;
+use shared_bus::BusManagerSimple;
+use ssd1306::{
+    I2CDisplayInterface, Ssd1306Async, mode::DisplayConfigAsync, rotation::DisplayRotation,
+    size::DisplaySize128x64,
+};
 use {esp_backtrace as _, esp_println as _};
 
 extern crate alloc;
@@ -20,7 +31,7 @@ extern crate alloc;
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[esp_rtos::main]
-async fn main(spawner: Spawner) -> ! {
+async fn main(_spawner: Spawner) -> ! {
     // generator version: 1.0.1
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -35,8 +46,31 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    let i2c_bus = I2c::new(
+        peripherals.I2C0,
+        I2cConfig::default().with_frequency(Rate::from_khz(400)),
+    )
+    .unwrap()
+    .with_scl(peripherals.GPIO8)
+    .with_sda(peripherals.GPIO9)
+    .into_async();
+
+    let i2c_bus = BusManagerSimple::new(i2c_bus);
+
+    let interface = I2CDisplayInterface::new(i2c_bus.acquire_i2c());
+    let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().await.unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello, Rust!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    display.flush().await.unwrap();
 
     loop {
         info!("Hello world!");
