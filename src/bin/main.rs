7ce7e835fc5fabb406019b4348a6fb45c1775da4
Adam M. Szalkowski <adam@szalkowski.de>
2025-12-22 15:02:52 +0100

    feat: humidity + bus sharing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,10 +6,15 @@
     holding buffers for the duration of a data transfer."
 )]
 
+use alloc::format;
 use defmt::info;
+use embassy_embedded_hal::shared_bus;
 use embassy_executor::Spawner;
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::mutex::Mutex;
 use embassy_time::{Duration, Timer};
-use embedded_graphics::mono_font::{MonoTextStyleBuilder, ascii::FONT_6X10};
+use embedded_aht20::{Aht20, DEFAULT_I2C_ADDRESS};
+use embedded_graphics::mono_font::{MonoTextStyleBuilder, iso_8859_1::FONT_6X10};
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::*;
 use embedded_graphics::text::{Baseline, Text};
@@ -17,7 +22,7 @@ use esp_hal::clock::CpuClock;
 use esp_hal::i2c::master::{Config as I2cConfig, I2c};
 use esp_hal::time::Rate;
 use esp_hal::timer::timg::TimerGroup;
-use shared_bus::BusManagerSimple;
+use shared_bus::asynch::i2c::I2cDevice;
 use ssd1306::{
     I2CDisplayInterface, Ssd1306Async, mode::DisplayConfigAsync, rotation::DisplayRotation,
     size::DisplaySize128x64,
@@ -51,15 +56,25 @@ async fn main(_spawner: Spawner) -> ! {
         I2cConfig::default().with_frequency(Rate::from_khz(400)),
     )
     .unwrap()
-    .with_scl(peripherals.GPIO8)
-    .with_sda(peripherals.GPIO9)
+    .with_scl(peripherals.GPIO18)
+    .with_sda(peripherals.GPIO19)
     .into_async();
+    let i2c_bus: Mutex<NoopRawMutex, _> = Mutex::new(i2c_bus);
 
-    let i2c_bus = BusManagerSimple::new(i2c_bus);
+    info!("Initializing sensor...");
+    let mut sensor = Aht20::new(
+        I2cDevice::new(&i2c_bus),
+        DEFAULT_I2C_ADDRESS,
+        embassy_time::Delay,
+    )
+    .await
+    .unwrap();
 
-    let interface = I2CDisplayInterface::new(i2c_bus.acquire_i2c());
+    info!("Initializing display...");
+    let interface = I2CDisplayInterface::new(I2cDevice::new(&i2c_bus));
     let mut display = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
+
     display.init().await.unwrap();
 
     let text_style = MonoTextStyleBuilder::new()
@@ -67,14 +82,42 @@ async fn main(_spawner: Spawner) -> ! {
         .text_color(BinaryColor::On)
         .build();
 
-    Text::with_baseline("Hello, Rust!", Point::zero(), text_style, Baseline::Top)
+    loop {
+        info!("Measuring temp/hum...");
+        let measurement = sensor.measure().await.unwrap();
+        info!(
+            "Temperature: {} °C, Relative humidity: {} %",
+            measurement.temperature.celsius(),
+            measurement.relative_humidity
+        );
+
+        display.clear_buffer();
+
+        Text::with_baseline("Hello, Rust!", Point::zero(), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        Text::with_baseline(
+            &format!("Temperature: {:.1} °C", measurement.temperature.celsius()),
+            Point::new(0, 16),
+            text_style,
+            Baseline::Top,
+        )
         .draw(&mut display)
         .unwrap();
-    display.flush().await.unwrap();
 
-    loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        Text::with_baseline(
+            &format!("rel. Humidity: {:.1} %", measurement.relative_humidity),
+            Point::new(0, 32),
+            text_style,
+            Baseline::Top,
+        )
+        .draw(&mut display)
+        .unwrap();
+        info!("Flushing display...");
+        display.flush().await.unwrap();
+
+        Timer::after(Duration::from_secs(5)).await;
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
